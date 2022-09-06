01bb6f6350cae909772dfdf0d3edc49a105406fd
tungthactda <tung.nguyen@tda.company>
2022-09-06 19:56:22 +0700

    type js

--- Buoi7-JS-1/index.js
+++ Buoi7-JS-1/index.js
@@ -1,4 +1,37 @@
-var a = 8;
-var b = 20; 
+// các cách khai báo biến trong js 
+var a = 10; // kiểu number
+var a = 50.6; // kiểu number
+console.log(a);
 
-alert( a + b)
+let name = 'Tung'; // kiểu string
+ name = "Hoang"; // kiểu string
+ console.log(name);
+let c = true;// or false : kiểu boolean;
+const PI = 3.14;
+console.log(PI);
+let car = {
+    color: 'red',
+    price: 1000000,
+    maxSpeed: 500,
+    isPay: false,
+} // kiểu object 
+
+let people = {
+    name: 'Tung',
+    age: 27,
+    sex: 'male'
+} // kiểu object 
+
+function getNumber() {
+    let isFan = 50;
+    if (isFan > 100) {
+        return 'tung là người nổi tiếng'
+    }else {
+        return 'Tung chả là gì'
+    }
+   
+} // kiểu function
+
+let undef = undefined // kiểu undefined
+
+console.log(typeof getNumber)
