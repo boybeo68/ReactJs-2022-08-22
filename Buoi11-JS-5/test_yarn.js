14003b8e8cf1a30c0584743b819feff577ea33ad
tungthactda <tung.nguyen@tda.company>
2022-09-27 19:00:43 +0700

    done 11

--- Buoi11-JS-5/test_yarn.js
+++ Buoi11-JS-5/test_yarn.js
@@ -1,11 +1,35 @@
 var readlineSync = require('readline-sync');
 const shortid = require('shortid');
 var fs = require('fs');
+var _ = require('lodash');
 
 let animals = ['Lion', 'Elephant', 'Crocodile', 'Giraffe', 'Hippo'];
 let readFile = fs.readFileSync('./yarn.txt', 'utf-8');
 fs.writeFileSync('./data.txt', 'tung da thay doi', 'utf-8');
 console.log(readFile);
+
+const students = [
+  {
+    id: 1,
+    name: 'tung',
+  },
+  {
+    id: 2,
+    name: 'tung2',
+  },
+  {
+    id: 3,
+    name: 'tung3',
+  },
+  {
+    id: 4,
+    name: 'tung4',
+  },
+];
+let data = _.remove(students, function (item) {
+  return _.indexOf([2, 3], item.id) !== -1;
+});
+console.log('bb data', data, students);
 // var userName = readlineSync.question('May I have your name? ');
 // console.log('Hi ' + userName + '!');
 // console.log(shortid.generate());
