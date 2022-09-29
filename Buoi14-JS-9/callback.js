f877988b3084fc4cab77ec098767726a55a661a5
tungthactda <tung.nguyen@tda.company>
2022-09-29 19:57:09 +0700

    promise

--- Buoi14-JS-9/callback.js
+++ Buoi14-JS-9/callback.js
@@ -1,5 +1,5 @@
 // Tính diện tích hình thang
-// (a + b) *h /2 : a= 10, b = 5, h= 7
+// (a + b) *h /2 *2 /4 + 8 + 2 / 6
 
 // phép công xử lý mất 3s
 // phép  nhân xử lý mất 2s
@@ -37,12 +37,22 @@ const chia = (a, b, cb) => {
     // return result;
   }, 5000);
 };
-
+// (a + b) *h /2 *2 /4 + 8 + 2 / 6
 const ketqua = (cb) => {
   cong(10, 5, (resultCong) => {
     nhan(resultCong, 7, (resultNhan) => {
       chia(resultNhan, 2, (resultChia) => {
-        cb(resultChia);
+        nhan(resultChia, 2, (nhan2) => {
+          chia(nhan2, 4, (chia4) => {
+            cong(chia4, 8, (cong8) => {
+              cong(cong8, 2, (cong2) => {
+                chia(cong2, 6, (chia6) => {
+                  cb(chia6);
+                });
+              });
+            });
+          });
+        });
       });
     });
   });
